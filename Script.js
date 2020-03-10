ff3477809ec48e7e863f1dcd9d7b1808c4ae97b8
Miss. R Magopane <u18239570@up.ac.za>
2020-03-10 14:57:16 +0200

    Rebone

--- Script.js
+++ Script.js
@@ -1,5 +1,8 @@
-function buttonFuntion(){
+function buttonFunction(){
     alert("I will be able to programme for my career path. And earn a lot of money. :)");
+}
+function buttonFun(){
+    alert("I can create my own business webpage for free.");
 }
 
 
